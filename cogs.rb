13f4afecad137caae5738f15a41df27b66bcec4b
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-29 23:56:05 +0100

    lowcomden works nicely

--- cogs.rb
+++ cogs.rb
@@ -18,7 +18,6 @@ class Cog
 
   def num_of_spins(cog1, cog2)
 
-    total = 0
     total = cog1.spin(cog1, cog2) 
     if total % cog1.points == 0
       result = total / cog1.points
@@ -28,12 +27,32 @@ class Cog
 
   def multi_num_of_spins(cog1, cog2, cog3)
 
-    total = 0
     total = cog1.multi_spin(cog1, cog2, cog3) 
     if total % cog1.points == 0
       result = total / cog1.points
       return result
     end
   end
+
+  def lowcomden(cog1, cog2, cog3)
+    result = cog1.multi_num_of_spins(cog1, cog2, cog3)
+    return result * cog1.points
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
