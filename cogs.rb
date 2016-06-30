db575017134fedeacf0631189bb6fd72535a306c
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-30 11:31:44 +0100

    total mess

--- cogs.rb
+++ cogs.rb
@@ -4,6 +4,7 @@ class Cog
 
   def initialize(points)
     @points = points
+    @results = []
   end
 
   def spin(cog1, cog2)
@@ -11,10 +12,25 @@ class Cog
     return result
   end
 
-  def multi_spin(cog1, cog2, cog3)
-    result = cog1.points * cog2.points * cog3.points
-    return result
+  def self.cog_map(cogs)
+    @results = cogs.map{ |cog| cog.points}
+    return @results
   end
+
+  def product(results)
+    final = 1.0
+    @results.each { |result| final *= result }
+    final
+  end
+  # def multi_spin(cogs)
+    
+    
+  #   for cog in cogs
+  #     cog.points
+  #   result = cog1.points * cog2.points * cog3.points
+  #   return result
+  # end
+  # end
 
   def num_of_spins(cog1, cog2)
 
@@ -25,21 +41,34 @@ class Cog
     end
   end
 
-  def multi_num_of_spins(cog1, cog2, cog3)
+  # def multi_num_of_spins(cog1, cog2, cog3)
 
-    total = cog1.multi_spin(cog1, cog2, cog3) 
-    if total % cog1.points == 0
-      result = total / cog1.points
-      return result
-    end
-  end
+  #   total = cog1.multi_spin(cog1, cog2, cog3) 
+  #   if total % cog1.points == 0
+  #     result = total / cog1.points
+  #     return result
+  #   end
+  # end
 
-  def lowcomden(cog1, cog2, cog3)
-    result = cog1.multi_num_of_spins(cog1, cog2, cog3)
-    return result * cog1.points
-  end
+  # def lowcomden(cog1, cog2, cog3)
+  #   result = cog1.multi_num_of_spins(cog1, cog2, cog3)
+  #   return result * cog1.points
+  # end
 
 
+  # def multi_num_of_spins(cog1,cogs)
+
+  #   cog_pot = []
+  #   cog_pot << cog1
+
+  #   cogs.delete(cog1)
+
+  #   total = cog1.multi_spin(cog_pot) 
+  #   if total % cog1.points == 0
+  #     result = total / cog1.points
+  #     return result
+  #   end
+  # end
 
 
 
--- specs/cog_spec.rb
+++ specs/cog_spec.rb
@@ -9,29 +9,44 @@ class TestCogs < MiniTest::Test
     @cog1 = Cog.new(5)
     @cog2 = Cog.new(3)
     @cog3 = Cog.new(7)
+
+    @cogs=[@cog1,@cog2,@cog3]
   end
 
-  def test_cog_points
-    assert_equal(5, @cog1.points)
+  # def test_cog_points
+  #   assert_equal(5, @cog1.points)
+  # end
+
+  # def test_spin
+  #   assert_equal(15, @cog1.spin(@cog1, @cog2))
+  # end
+
+  # def test_num_of_spins
+  #   # binding.pry
+  #   assert_equal(3, @cog1.num_of_spins(@cog1, @cog2))
+  # end
+
+  # def test_multi_num_of_spins
+  #   # binding.pry
+  #   assert_equal(21, @cog1.multi_num_of_spins(@cog1, @cog2, @cog3))
+  # end  
+
+  # def test_multi_num_of_spins
+  #   # binding.pry
+  #   assert_equal(21, @cog1.multi_num_of_spins(@cog1, @cog2, @cog3))
+  # end
+
+  # def test_lowcomden
+  #   assert_equal( 105, @cog1.lowcomden(@cog1, @cog2, @cog3))
+  # end
+
+  def test_product
+    cog_map(@cogs)
+    binding.pry
+    assert_equal(171, product(result))
   end
 
-  def test_spin
-    assert_equal(15, @cog1.spin(@cog1, @cog2))
-  end
 
-  def test_num_of_spins
-    # binding.pry
-    assert_equal(3, @cog1.num_of_spins(@cog1, @cog2))
-  end
-
-  def test_multi_num_of_spins
-    # binding.pry
-    assert_equal(21, @cog1.multi_num_of_spins(@cog1, @cog2, @cog3))
-  end
-
-  def test_lowcomden
-    assert_equal( 105, @cog1.lowcomden(@cog1, @cog2, @cog3))
-  end
 
 
 
